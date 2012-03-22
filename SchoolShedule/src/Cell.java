20ae29e4822be3c582196e951565443ab641fbc8
kosyo <kosyokk@gmail.com>
2012-03-22 13:23:22 +0200

    This class is for Google Spreadsheet API. This is class contains cordinates of cell.

---
+++ SchoolShedule/src/Cell.java
@@ -0,0 +1,26 @@
+public class Cell {
+	private int row;
+	private int col;
+	
+	Cell(int row, int col) {
+		this.row=row;
+		this.col=col;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getCol() {
+		return col;
+	}
+
+	public void setCol(int col) {
+		this.col = col;
+	}
+	
+}
