f55c71a9d7efc50bd3cbfe1a0c1d4be376f38a30
kosyo <kosyokk@gmail.com>
2012-04-18 15:51:48 +0300

    Added test project

---
+++ SchoolShedule/src/org/elsys/test/TestSpreadsheet.java
@@ -0,0 +1,14 @@
+package org.elsys.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSpreadsheet {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
