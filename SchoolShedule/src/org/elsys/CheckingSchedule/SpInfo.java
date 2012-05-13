40a02cd2a37d29ca220dd0819e0ba5a3dfa0ee44
kosyo <kosyokk@gmail.com>
2012-05-13 17:44:00 +0300

    SpInfo contains information about spreadsheet columns and rows

---
+++ SchoolShedule/src/org/elsys/CheckingSchedule/SpInfo.java
@@ -0,0 +1,20 @@
+package org.elsys.CheckingSchedule;
+
+public class SpInfo {
+	public static final int rowForYear = 1;
+	public static final int colForYear = 14;
+	public static final int rowForTerm = 1;
+	public static final int colForTerm = 13;
+	public static final int classRow = 2;
+	public static final int firstClassCol = 4;
+	public static final int secondClassCol = 13;
+	public static final int thirdClassCol = 22;
+	public static final int fourthClassCol = 31;
+	public static final int startClassRow = 2;
+	public static final int endClassRow = 50;
+	public static final int firstClassSubjectsCol = 4;
+	public static final int secondClassSubjectsCol = 13;
+	public static final int thirdClassSubjectsCol = 22;
+	public static final int fourthClassSubjectsCol = 31;
+
+}
